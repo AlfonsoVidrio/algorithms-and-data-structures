d40a6a0a1b33ea310289ea26ea330fa2b4ae2ea7
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-18 00:49:17 -0600

    test: add unit tests for 'TwoSum' solution

--- ArraysStringsHashTables/src/test/java/com/alfonsovidrio/algorithmsdatastructures/exercise2/TwoSumTest.java
+++ ArraysStringsHashTables/src/test/java/com/alfonsovidrio/algorithmsdatastructures/exercise2/TwoSumTest.java
@@ -1,4 +1,17 @@
+package com.alfonsovidrio.algorithmsdatastructures.exercise2;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TwoSumTest {
-  
+
+    @Test
+    void twoSum() {
+        int[] array = new int[] {9, 2, 5, 6};
+        TwoSum twoSum = new TwoSum();
+        int [] result = twoSum.twoSum(array, 7);
+        assertTrue((result[0] == 1 && result[1] == 2) || (result[0] == 2 && result[1] == 1));
+        assertEquals(null, twoSum.twoSum(array,  50));
+    }
 }
