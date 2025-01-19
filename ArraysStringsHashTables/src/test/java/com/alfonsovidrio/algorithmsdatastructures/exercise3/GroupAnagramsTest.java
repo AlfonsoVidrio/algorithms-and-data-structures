7a50cf5b2903a4a182061a6dcac2e65894f488d3
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-18 23:39:32 -0600

    test: add unit tests for 'GroupAnagrams' solution

--- ArraysStringsHashTables/src/test/java/com/alfonsovidrio/algorithmsdatastructures/exercise3/GroupAnagramsTest.java
+++ ArraysStringsHashTables/src/test/java/com/alfonsovidrio/algorithmsdatastructures/exercise3/GroupAnagramsTest.java
@@ -1,4 +1,32 @@
+package com.alfonsovidrio.algorithmsdatastructures.exercise3;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GroupAnagramsTest {
-  
+
+    @Test
+    void groupAnagrams() {
+        List<List<String>> empty = GroupAnagrams.groupAnagrams(new String[] {});
+        assertTrue(empty.isEmpty());
+
+        List<List<String>>anagrams = GroupAnagrams.groupAnagrams(new String[] {"saco", "arresto", "programa", "rastreo", "caso"});
+
+        assertTrue(anagrams.size() == 3); // [["saco", "caso"], ["arresto", "rastreo"], ["programa"]]
+        assertTrue(containsAll(anagrams, List.of("programa")));
+        assertTrue(containsAll(anagrams, List.of("caso","saco")));
+        assertTrue(containsAll(anagrams, List.of("arresto", "rastreo")));
+    }
+
+    private boolean containsAll(List<List<String>> anagrams, List<String> group) {
+        for (List<String> g : anagrams) {
+            if (g.containsAll(group)) return true;
+        }
+        return false;
+    }
+
+
 }
