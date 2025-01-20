6c4544baff0fac627b537814702d9c2515a1f1fa
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-20 17:23:31 -0600

    feat: add Node class to represent a node in the linked list

---
+++ LinkedLists/src/main/java/com/alfonsovidrio/algorithmsdatastructures/linkedlists/Node.java
@@ -0,0 +1,10 @@
+package com.alfonsovidrio.algorithmsdatastructures.linkedlists;
+
+public class Node {
+    public Node next;
+    public int value;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
