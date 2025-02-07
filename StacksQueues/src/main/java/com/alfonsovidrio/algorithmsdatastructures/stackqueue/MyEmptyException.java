7284e0b74a2e033a519476ac1ab3374563e53fbf
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-02-07 16:37:19 -0600

    feat: add custom exception for empty stack or queue

---
+++ StacksQueues/src/main/java/com/alfonsovidrio/algorithmsdatastructures/stackqueue/MyEmptyException.java
@@ -0,0 +1,7 @@
+package com.alfonsovidrio.algorithmsdatastructures.stackqueue;
+
+public class MyEmptyException extends RuntimeException {
+    public MyEmptyException(String message) {
+        super(message);
+    }
+}
